94aaf032ca54a780ba52e70e490aaa042fcbb792
NurullohMahmud <nurullomahmud@gmail.com>
2024-03-05 19:40:23 -0600

    swagger

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     MyTokenObtainPairView, 
+    TokenRefreshView,
     UserRegisterAPIView, 
 )
 
 urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', UserRegisterAPIView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .models import CustomUser
 
